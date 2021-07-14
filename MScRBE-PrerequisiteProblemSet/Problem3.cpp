119133058079cad2058fd5e8aa9d7203ae3b6c50
Aditya Mehrotra <aditya.mehrotra97@gmail.com>
2021-07-15 02:06:33 +0530

    Problem3

--- MScRBE-PrerequisiteProblemSet/Problem3.cpp
+++ MScRBE-PrerequisiteProblemSet/Problem3.cpp
@@ -5,58 +5,79 @@
 
 using namespace std;
 
-bool isValid(string s) {
-    stack <char> expression;
-    bool validity = false;
-    if (s.length() < 2) return validity;
-    
-    for (int i = 0; i< s.length(); i++) {
-        if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
-            if (expression.size() == 0) return validity;
-            switch(s[i]){
-                case ')':
-                    if (expression.top() != '(') return validity;
-                    expression.pop();
-                    break;
-                case '}':
-                    if (expression.top() != '{') return validity;
-                    expression.pop();
-                    break;
-                case ']':
-                    if (expression.top() != '[') return validity;
-                    expression.pop();
-                    break;
-                default:
-                    return validity;
-            }
+double evaluate(string s) {
+
+    string compactExpression = "";
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' '){
+            compactExpression = compactExpression + s[i];
         }
-        else
-            expression.push(s[i]);
     }
-    if (!expression.size()) 
-        validity = true;
-    return validity;
-}
 
-double evaluate() {
+    string semiStatement = ""; // Do parantheses first
+    for (int i = 0; i < compactExpression.length(); i++)
+    {
+        if (compactExpression[i] == '(')
+        {
+            int paranthesis = 1;
+            string subExpression;
+            i++;
+            while (true)
+            {
+                if (compactExpression[i] == '(') {
+                    paranthesis++;
+                }
 
+                else if (compactExpression[i] == ')') {
+                    paranthesis--;
+                    if (paranthesis == 0)
+                    {
+                        i++;
+                        break;
+                    }
+                }
+                subExpression += compactExpression[i];
+                i++;
+            }
+            semiStatement += to_string(evaluate(subExpression));
+        }
+        semiStatement += compactExpression[i];
+    }
+
+    for (int i = 0; i < semiStatement.length(); i++)
+    {
+        if (semiStatement[i] == '*') {
+            return evaluate(semiStatement.substr(0, i)) * evaluate(semiStatement.substr(i+1, semiStatement.length()-i-1));
+        } 
+        
+        else if (semiStatement[i] == '/') {
+            return evaluate(semiStatement.substr(0, i)) / evaluate(semiStatement.substr(i+1, semiStatement.length()-i-1));
+        }
+
+        else if (semiStatement[i] == '+')
+        {
+            return evaluate(semiStatement.substr(0, i)) + evaluate(semiStatement.substr(i+1, semiStatement.length()-i-1));
+        } 
+        
+        else if (semiStatement[i] == '-') {
+            return evaluate(semiStatement.substr(0, i)) - evaluate(semiStatement.substr(i+1, semiStatement.length()-i-1));
+        }
+    }
+
+    return stod(semiStatement.c_str());
 }
 
 int main() {
 
-    map<char, string> Operator;
-
-    Operator['+'] = "Add";
-    Operator['-'] = "Subtract";
-    Operator['/'] = "Divide";
-    Operator['*'] = "Multiply";
-    Operator['%'] = "Modulus";
-
     cout<<"Enter an expression within double quotes (\"\"): ";
     string s;
     getline(cin, s);
-    if(isValid(s)) {
-
+    if(s[0] == '\"' && s[s.length() - 1] == '\"') {
+        s = s.substr(1, s.length() -2);
+        cout<<evaluate(s);
     }
+    else
+        cout<<"Expression must inculude quotes";
     return 0;
 }
